caba1565e9f20a1f70f8ad13bc446509bc34c1a1
Craig Fielder <craig.fielder@teslagovernment.com>
2024-09-06 00:04:14 -0400

    fix: build script

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ ripgrep
 intel-media-driver
 )
 
-sudo rpm-ostree install \
+rpm-ostree install \
         https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
         https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm
 
