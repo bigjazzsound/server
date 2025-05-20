abce3d1c3030fdefce77fd6601817f8c98d511f0
Craig Fielder <craig.fielder@teslagovernment.com>
2025-05-20 10:09:21 -0400

    fix: do not enable k3s

--- build.sh
+++ build.sh
@@ -30,6 +30,7 @@ rpm-ostree install ${pkgs[*]}
 
 curl -sfL https://get.k3s.io | \
         INSTALL_K3S_SKIP_START=true \
+        INSTALL_K3S_SKIP_ENABLE=true \
         INSTALL_K3S_SKIP_SELINUX_RPM=true \
         INSTALL_K3S_BIN_DIR=/usr/bin \
         sh -s - \
